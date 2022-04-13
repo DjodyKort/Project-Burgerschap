58a384d90f66bc81ad343f8273b86c0e8f373007
Djody Kort <507946@vistacollege.nl>
2022-04-13 10:25:35 +0200

    PHPSTORM SHITd
    d

--- html/film.php
+++ html/film.php
@@ -7,123 +7,123 @@
 </head>
 
 <body>
-      <!-- Navbar -->
-      <div class='container1'>
-        <ul class='ul1'>
-            <li class='li1'> <a href='../index.php'> Home </a> </li>
-            <li class='li1'> <a href='./christendom.php'>Christendom</a></li>
-            <li class='li1'> <a href='./islam.php'>Islam</a></li>
-            <li class='li1'> <a href='./jodendom.php'>Jodendom</a></li>
-            <li class='li1'> <a class='select' href='./film.php'>Film</a></li>
-            <li class='li1'> <a href='./blogs/volkenkundemuseum-blog.php'>Museum Blog</a></li>
-            <li class='li1'> <a href='./blogs/cultuur-blog.php'>Cultuur Blog</a></li>
-        </ul>
-    </div>
-    <!-- End navbar -->
-    <h2>Watani – My Homeland</h2>
-    <img class='center' src='../images/watani.jpeg'  height='250px' alt='Error: Look for watani.'>
-    <h4>
-        Veel mensen en dieren zijn bang voor vuurwerk.<br />
-        Waarom denk je dat vluchtelingen bang zijn voor vuurwerk?
-    </h4>
-    <p>het laat ze denken aan de oorlog (geweerschoten uit syrië). Veel ouders zeggen tegen hun kinderen dat dat
-        vuurwerk is in plaats van geweerschoten.</p>
-    <h4>
-        In de film wordt gezegd ‘Ik zou in Hitler veranderen’.<br />
-        Slachtoffers worden daders. Het gaat hier om een oorlogssituatie.<br />
-        Ken je een andere situatie waar het helaas ook vaak voorkomt dat slachtoffers daders worden?
-    </h4>
-    <p>Pesten is een fenomeen dat vaak voorkomt op de basisschool / middelbare school.
-        Meestal worden de gepeste kinderen later zelf pestkoppen omdat ze niet meer in zo’n
-        kwetsbare positie willen komen en dan zelf macht wilt uiten op andere mensen.
-    </p>
-    <h4>
-        Recht en onrecht/normen en waarden:<br />
-        Ook in deze situatie vindt een van de kinderen het onaanvaardbaar om iets uit andermans <br />
-        huis mee te nemen. Op tv zie je plunderingen en vernietiging van andermans eigendom. <br />
-        Hoe kun je dat rijmen met normen en waarden in onze maatschappij en de situatie in de documentaire?
-    </h4>
-    <p>
-        Onze situatie is vooral anders als dan die van hun. Hun moeten steeds verwisselen/veranderen van huis. Dus soms
-        blijven
-        spullen achtergebleven door andere mensen (ongebruikt). Dus in sommige situaties kunnen die spullen gewoon
-        meegenomen
-        worden zonder dat iets of iemand het zal merken.<br />
-        <br />
-        Bij onze normen en waarden is het onacceptabel aangezien iedereen hier fatsoenlijk genoegen welvaart heeft om
-        het zelf
-        te betalen. Terwijl dat bij hun totaal niet kan.<br />
-        <br />
-        Dus voor hun is het gewoon een compleet andere situatie en dus “normaal”
-    </p>
-    <h4>Wat valt je op aan de spelende kinderen bij ISIS?</h4>
-    <p>
-        Kinderen bij ISIS “spelen” een toneelstuk waarbij ze terroristen van ISIS nadoen.
-        Ze doen dan slachtoffers “vermoorden” tijdens het toneelstuk. Ze zien er ook depressief uit.
-    </p>
-    <h4>
-        Waarom zijn mobiele telefoons zo belangrijk?
-    </h4>
-    <p>
-        Het heeft belachelijk veel sentimentele waarde voor hun, omdat ze hun familie en contacten bij
-        haar heeft. Kort gezegd alles wat ze nodig heeft dat ze “haar universum” noemt.
-    </p>
-    <h4>
-        In de film wordt propaganda voor Goslar getoond. Klopt de informatie die men krijgt <br />
-        (oude mensen sterven, Duitsers houden van Syriërs)? Wie zou een dergelijk filmpje gemaakt hebben?
-    </h4>
-    <p>
-        Nee, de informatie klopt helaas niet. Het is niet accuraat aangezien de meeste Duitsers juist tegen
-        vluchtelingen zijn.
-        Het filmpje zou gemaakt kunnen zijn door iemand die daar woont. In de documentaire werd wel duidelijk afgebeeld
-        dat het
-        een Youtube filmpje was. Het enigste wat we nu kunnen doen is speculeren over wat voor persoon/maatschappij het
-        heeft
-        gemaakt.
-    </p>
-    <h4>
-        Er is sprake van (religieuze) intolerantie/vooroordelen in deze film. Noem twee voorbeelden.
-    </h4>
-    <p>
-        Ze denken dat moslims potentiële terroristen zijn, ook denken mensen vaak dat vluchtelingen vaak geen baan
-        willen krijgen.
-    </p>
-    <h4>
-        Wat zien de Syriërs als verschillen met hun eigen cultuur als ze in Duitsland zijn gearriveerd?
-    </h4>
-    <p>
-        Geen hoofddoeken, iedereen heeft kort haar.
-    </p>
-    <h4>
-        Welke tegenreacties zijn er bij een aantal Duitsers?
-    </h4>
-    <p>
-        “Ga terug naar je eigen land” - jullie krijgen geld maar jullie werken niet.
-    </p>
-    <h4>
-        Wat denk jij dat belangrijk is om als mensen van een verschillende achtergrond goed samen te kunnen leven?
-    </h4>
-    <p>
-        Elkaar begrijpen. En geen vooroordelen maken voor elkaar.
-    </p>
-    <h4>
-        Nederland heeft altijd invloed gehad van diverse volkeren: van Romeinen tot Vikingen,<br/> 
-        de handel met de
-        Hanzesteden en de wereldhandel vanaf de 17e eeuw.<br />
-        Noem eens 5 zaken die we zonder vreemde/buitenlandse invloeden niet hadden?
-    </h4>
-    <p>
-        Welvaart(handel), restaurants van verschillende culturen, Gastarbeiders(slaven), spiegels(romeinen),
-        koffie(arabië),
-        specerijen(azië).
-    </p>
-    <h4>
-        Waarom heeft Nederland/Europa ook nu immigranten nodig?
-    </h4>
-    <p>
-        Arbeid en een mogelijkheid om nieuwe culturele dingen over te nemen.
-    </p>
-    <br /><br /><br /><br />
+<!-- Navbar -->
+<div class='container1'>
+    <ul class='ul1'>
+        <li class='li1'><a href='../index.php'> Home </a></li>
+        <li class='li1'><a href='./christendom.php'>Christendom</a></li>
+        <li class='li1'><a href='./islam.php'>Islam</a></li>
+        <li class='li1'><a href='./jodendom.php'>Jodendom</a></li>
+        <li class='li1'><a class='select' href='./film.php'>Film</a></li>
+        <li class='li1'><a href='./blogs/volkenkundemuseum-blog.php'>Museum Blog</a></li>
+        <li class='li1'><a href='./blogs/cultuur-blog.php'>Cultuur Blog</a></li>
+    </ul>
+</div>
+<!-- End navbar -->
+<h2>Watani – My Homeland</h2>
+<img class='center' src='../images/watani.jpeg' height='250px' alt='Error: Look for watani.'>
+<h4>
+    Veel mensen en dieren zijn bang voor vuurwerk.<br/>
+    Waarom denk je dat vluchtelingen bang zijn voor vuurwerk?
+</h4>
+<p>het laat ze denken aan de oorlog (geweerschoten uit syrië). Veel ouders zeggen tegen hun kinderen dat dat
+    vuurwerk is in plaats van geweerschoten.</p>
+<h4>
+    In de film wordt gezegd ‘Ik zou in Hitler veranderen’.<br/>
+    Slachtoffers worden daders. Het gaat hier om een oorlogssituatie.<br/>
+    Ken je een andere situatie waar het helaas ook vaak voorkomt dat slachtoffers daders worden?
+</h4>
+<p>Pesten is een fenomeen dat vaak voorkomt op de basisschool / middelbare school.
+    Meestal worden de gepeste kinderen later zelf pestkoppen omdat ze niet meer in zo’n
+    kwetsbare positie willen komen en dan zelf macht wilt uiten op andere mensen.
+</p>
+<h4>
+    Recht en onrecht/normen en waarden:<br/>
+    Ook in deze situatie vindt een van de kinderen het onaanvaardbaar om iets uit andermans <br/>
+    huis mee te nemen. Op tv zie je plunderingen en vernietiging van andermans eigendom. <br/>
+    Hoe kun je dat rijmen met normen en waarden in onze maatschappij en de situatie in de documentaire?
+</h4>
+<p>
+    Onze situatie is vooral anders als dan die van hun. Hun moeten steeds verwisselen/veranderen van huis. Dus soms
+    blijven
+    spullen achtergebleven door andere mensen (ongebruikt). Dus in sommige situaties kunnen die spullen gewoon
+    meegenomen
+    worden zonder dat iets of iemand het zal merken.<br/>
+    <br/>
+    Bij onze normen en waarden is het onacceptabel aangezien iedereen hier fatsoenlijk genoegen welvaart heeft om
+    het zelf
+    te betalen. Terwijl dat bij hun totaal niet kan.<br/>
+    <br/>
+    Dus voor hun is het gewoon een compleet andere situatie en dus “normaal”
+</p>
+<h4>Wat valt je op aan de spelende kinderen bij ISIS?</h4>
+<p>
+    Kinderen bij ISIS “spelen” een toneelstuk waarbij ze terroristen van ISIS nadoen.
+    Ze doen dan slachtoffers “vermoorden” tijdens het toneelstuk. Ze zien er ook depressief uit.
+</p>
+<h4>
+    Waarom zijn mobiele telefoons zo belangrijk?
+</h4>
+<p>
+    Het heeft belachelijk veel sentimentele waarde voor hun, omdat ze hun familie en contacten bij
+    haar heeft. Kort gezegd alles wat ze nodig heeft dat ze “haar universum” noemt.
+</p>
+<h4>
+    In de film wordt propaganda voor Goslar getoond. Klopt de informatie die men krijgt <br/>
+    (oude mensen sterven, Duitsers houden van Syriërs)? Wie zou een dergelijk filmpje gemaakt hebben?
+</h4>
+<p>
+    Nee, de informatie klopt helaas niet. Het is niet accuraat aangezien de meeste Duitsers juist tegen
+    vluchtelingen zijn.
+    Het filmpje zou gemaakt kunnen zijn door iemand die daar woont. In de documentaire werd wel duidelijk afgebeeld
+    dat het
+    een Youtube filmpje was. Het enigste wat we nu kunnen doen is speculeren over wat voor persoon/maatschappij het
+    heeft
+    gemaakt.
+</p>
+<h4>
+    Er is sprake van (religieuze) intolerantie/vooroordelen in deze film. Noem twee voorbeelden.
+</h4>
+<p>
+    Ze denken dat moslims potentiële terroristen zijn, ook denken mensen vaak dat vluchtelingen vaak geen baan
+    willen krijgen.
+</p>
+<h4>
+    Wat zien de Syriërs als verschillen met hun eigen cultuur als ze in Duitsland zijn gearriveerd?
+</h4>
+<p>
+    Geen hoofddoeken, iedereen heeft kort haar.
+</p>
+<h4>
+    Welke tegenreacties zijn er bij een aantal Duitsers?
+</h4>
+<p>
+    “Ga terug naar je eigen land” - jullie krijgen geld maar jullie werken niet.
+</p>
+<h4>
+    Wat denk jij dat belangrijk is om als mensen van een verschillende achtergrond goed samen te kunnen leven?
+</h4>
+<p>
+    Elkaar begrijpen. En geen vooroordelen maken voor elkaar.
+</p>
+<h4>
+    Nederland heeft altijd invloed gehad van diverse volkeren: van Romeinen tot Vikingen,<br/>
+    de handel met de
+    Hanzesteden en de wereldhandel vanaf de 17e eeuw.<br/>
+    Noem eens 5 zaken die we zonder vreemde/buitenlandse invloeden niet hadden?
+</h4>
+<p>
+    Welvaart(handel), restaurants van verschillende culturen, Gastarbeiders(slaven), spiegels(romeinen),
+    koffie(arabië),
+    specerijen(azië).
+</p>
+<h4>
+    Waarom heeft Nederland/Europa ook nu immigranten nodig?
+</h4>
+<p>
+    Arbeid en een mogelijkheid om nieuwe culturele dingen over te nemen.
+</p>
+<br/><br/><br/><br/>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,51 +1,53 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <link rel='stylesheet' href='./css/style.css'>
-        <title>Burgerschap Project - Welkom</title>
-    </head>
-  
-    <body class='index-body'>
-        <!-- Navbar -->
-    <div class='container1'>
-        <ul class='ul1'>
-            <li class='li1'> <a class='select' href='./index.php'> Home </a> </li>
-            <li class='li1'> <a href='./html/christendom.php'>Christendom</a></li>
-            <li class='li1'> <a href='./html/islam.php'>Islam</a></li>
-            <li class='li1'> <a href='./html/jodendom.php'>Jodendom</a></li>
-            <li class='li1'> <a href='./html/film.php'>Film</a></li>
-            <li class='li1'> <a href='./html/blogs/volkenkundemuseum-blog.php'>Museum Blog</a></li>
-            <li class='li1'> <a href='./html/blogs/cultuur-blog.php'>Cultuur Blog</a></li>
-            </ul>
-            </div>
-    <!-- End navbar -->
-        <h1>Welkom!</h1>
-        <p>
-            Deze site gaat over het algemene onderwerp Religie. Hierin beantwoorden we vragen zoals:
-            <br /><br />
-            hoeveel goden hebben ze?<br />
-            waar geloven ze in?<br />
-            hoe leven ze? / welke tradities hebben ze?<br />
-            welke feesten hebben ze?<br />
-            welke tradities/ gewoontes?<br />
-            speciale kenmerken?<br />
-            <br />
-            Ook staat hieronder de overeenkomsten van onze uitgekozen religies
-            (Joden, Christenen en Moslims)
-        </p>
-        <img style='width:700px;' src='./images/religie.png' alt='Error: Look for Religie' class='center'>
+<head>
+    <link rel='stylesheet' href='./css/style.css'>
+    <title>Burgerschap Project - Welkom</title>
+</head>
 
-        <h2>Welke overeenkomsten en verschillen zijn er tussen de religies?</h2>
-        <p>
-            Afgezien van de overeenkomsten, hebben jodendom, christendom en islam veel fundamentele verschillen. Deze drie
-            religieszijn gebaseerd op het geloof in één God en hebben eerbied voor het heilige boek dat het woord van God bevat. Alledrie
-            namen ook de Messias aan.<br/>
-            <br />
-            Voor christenen wordt Jezus gezien als de Messias ("Christus"), in het Jodendom is de
-            Messias nog niet gekomen, en in de islam is al-Mahdi een Messias die naar de aarde zal komen en Kom tot vrede en<br />
-            gerechtigheid brengen.
-        </p>
-        <br /><br /><br /><br />
-    </body>
+<body class='index-body'>
+<!-- Navbar -->
+<div class='container1'>
+    <ul class='ul1'>
+        <li class='li1'><a class='select' href='./index.php'> Home </a></li>
+        <li class='li1'><a href='./html/christendom.php'>Christendom</a></li>
+        <li class='li1'><a href='./html/islam.php'>Islam</a></li>
+        <li class='li1'><a href='./html/jodendom.php'>Jodendom</a></li>
+        <li class='li1'><a href='./html/film.php'>Film</a></li>
+        <li class='li1'><a href='./html/blogs/volkenkundemuseum-blog.php'>Museum Blog</a></li>
+        <li class='li1'><a href='./html/blogs/cultuur-blog.php'>Cultuur Blog</a></li>
+    </ul>
+</div>
+<!-- End navbar -->
+<h1>Welkom!</h1>
+<p>
+    Deze site gaat over het algemene onderwerp Religie. Hierin beantwoorden we vragen zoals:
+    <br/><br/>
+    hoeveel goden hebben ze?<br/>
+    waar geloven ze in?<br/>
+    hoe leven ze? / welke tradities hebben ze?<br/>
+    welke feesten hebben ze?<br/>
+    welke tradities/ gewoontes?<br/>
+    speciale kenmerken?<br/>
+    <br/>
+    Ook staat hieronder de overeenkomsten van onze uitgekozen religies
+    (Joden, Christenen en Moslims)
+</p>
+<img style='width:700px;' src='./images/religie.png' alt='Error: Look for Religie' class='center'>
+
+<h2>Welke overeenkomsten en verschillen zijn er tussen de religies?</h2>
+<p>
+    Afgezien van de overeenkomsten, hebben jodendom, christendom en islam veel fundamentele verschillen. Deze drie
+    religieszijn gebaseerd op het geloof in één God en hebben eerbied voor het heilige boek dat het woord van God bevat.
+    Alledrie
+    namen ook de Messias aan.<br/>
+    <br/>
+    Voor christenen wordt Jezus gezien als de Messias ("Christus"), in het Jodendom is de
+    Messias nog niet gekomen, en in de islam is al-Mahdi een Messias die naar de aarde zal komen en Kom tot vrede
+    en<br/>
+    gerechtigheid brengen.
+</p>
+<br/><br/><br/><br/>
+</body>
 </html>
